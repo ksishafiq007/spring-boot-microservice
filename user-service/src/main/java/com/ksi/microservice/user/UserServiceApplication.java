cb2ba1e2532c833995753ba1aecb04f61dac8bb5
ksishafiq007 <ksishafiq007@gmail.com>
2021-03-17 15:58:01 +0600

    add department and user config

--- user-service/src/main/java/com/ksi/microservice/user/UserServiceApplication.java
+++ user-service/src/main/java/com/ksi/microservice/user/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.ksi.microservice.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ public class UserServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
--- user-service/src/main/java/com/ksi/microservice/user/service/UserService.java
+++ user-service/src/main/java/com/ksi/microservice/user/service/UserService.java
@@ -28,7 +28,7 @@ public class UserService {
         //log.ingo("Inside getUserWithDepartment of UserService")
         ResponseTemplateVO vo=new ResponseTemplateVO();
         User user=userRepository.findByUserId(userId);
-        Department department=restTemplate.getForObject("http://localhost:9001/departments/"+user.getDepartmentId(),Department.class);
+        Department department=restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/"+user.getDepartmentId(),Department.class);
         vo.setUser(user);
         vo.setDepartment(department);
         return vo;
