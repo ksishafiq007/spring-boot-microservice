f22ef15eb48343589ba10b54190a34f19b92cdfd
ksishafiq007 <ksishafiq007@gmail.com>
2021-03-19 01:33:58 +0600

    cloud config add

---
+++ cloud-config-server/src/test/java/com/ksi/microservice/cloud/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksi.microservice.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
